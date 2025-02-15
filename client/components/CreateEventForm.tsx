93b6ad5b63f431d12557d62fb2ba1a13f67e5c2b
Jacob Lin <jal041@ucsd.edu>
2025-02-14 23:06:34 -0800

    modified front end to have start end time, location

--- client/components/CreateEventForm.tsx
+++ client/components/CreateEventForm.tsx
@@ -16,8 +16,6 @@ export default function CreateEventForm(props: CreateEventFormProps) {
 
     const styles = isDesktop ? stylesDesktop : stylesMobile;
 
-    console.log(isDesktop)
-
     const {
         currentMonth,
         selectedDate,
@@ -31,23 +29,13 @@ export default function CreateEventForm(props: CreateEventFormProps) {
     return (
         <View style={styles.background}>
             <View style={styles.container}>
-                <View style={styles.titleContainer}>
-                    <TextInput 
-                        style={styles.title} 
-                        placeholder="Event Name" 
-                        placeholderTextColor="rgba(0, 0, 0, 0.5)"
-                    />
-                </View>
-                <View style={styles.dateOccupancyContainer}>
-                    <View style={styles.dateContainer}>
+                <View style={styles.titleOccupancyContainer}>
+                    <View style={styles.titleContainer}>
                         <TextInput 
-                            style={styles.date} 
-                            placeholder="00/00/00"
+                            style={styles.title} 
+                            placeholder="Event Name" 
                             placeholderTextColor="rgba(0, 0, 0, 0.5)"
                         />
-                        <TouchableOpacity >
-                            <Image style={styles.calendarIcon} source={require("../assets/images/calendar-icon.png")} />
-                        </TouchableOpacity>
                     </View>
                     <View style={styles.occupancyContainer}>
                         <TextInput 
@@ -57,6 +45,38 @@ export default function CreateEventForm(props: CreateEventFormProps) {
                         />
                         <Image style={styles.personIcon} source={require("../assets/images/person-icon.png")}/>
                     </View>
+                </View>
+                <View style={styles.startEndContainer}>
+                    <View style={styles.dateContainer}>
+                        <TextInput 
+                            style={styles.date} 
+                            placeholder="00/00 00:00am"
+                            placeholderTextColor="rgba(0, 0, 0, 0.5)"
+                        />
+                        <TouchableOpacity >
+                            <Image style={styles.calendarIcon} source={require("../assets/images/calendar-icon.png")} />
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.dateContainer}>
+                        <TextInput 
+                            style={styles.date} 
+                            placeholder="00/00 00:00pm"
+                            placeholderTextColor="rgba(0, 0, 0, 0.5)"
+                        />
+                        <TouchableOpacity >
+                            <Image style={styles.calendarIcon} source={require("../assets/images/calendar-icon.png")} />
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                <View style={styles.locationContainer}>
+                    <TextInput 
+                        style={styles.location} 
+                        placeholder="Somewhere, Someplace"
+                        placeholderTextColor="rgba(0, 0, 0, 0.5)"
+                    />
+                    <TouchableOpacity>
+                        <Image style={styles.mapsIcon} source={require("../assets/images/maps-icon.png")} />
+                    </TouchableOpacity>
                 </View>
                 <View style={styles.tagsContainer}>
 
@@ -98,40 +118,23 @@ const stylesMobile = StyleSheet.create({
         flexDirection: "column",
         gap: 20
     },
+    titleOccupancyContainer: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        gap: 20
+    },
     titleContainer: {
         backgroundColor: COLORS.blueGray,
         height: 100,
         borderRadius: 20,
-        width: 400,
         justifyContent: "center",
-        paddingHorizontal: 20
+        paddingHorizontal: 20,
+        flex: 1
     },
     title: {
-        fontSize: 48
-    },
-    dateOccupancyContainer: {
-        display: "flex",
-        flexDirection: "row",
-        gap: 20
-    },
-    dateContainer: {
-        backgroundColor: COLORS.blueGray,
-        height: 100,
-        width: 300,
-        borderRadius: 20,
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-        paddingHorizontal: 20
-    },
-    date: {
         fontSize: 48,
-        width: 200
-    },
-    calendarIcon: {
-        height: 50,
-        width: 50
+        fontFamily: "'Zain', sans-serif"
     },
     occupancyContainer: {
         backgroundColor: COLORS.blueGray,
@@ -146,9 +149,54 @@ const stylesMobile = StyleSheet.create({
     },
     occupancy: {
         fontSize: 48,
+        fontFamily: "'Zain', sans-serif",
         width: 100
     },
+    startEndContainer: {
+        display: "flex",
+        flexDirection: "row",
+        gap: 20
+    },
+    dateContainer: {
+        backgroundColor: COLORS.blueGray,
+        height: 100,
+        flex: 1,
+        borderRadius: 20,
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        paddingHorizontal: 20
+    },
+    date: {
+        fontSize: 36,
+        fontFamily: "'Zain', sans-serif",
+        width: 200
+    },
+    calendarIcon: {
+        height: 50,
+        width: 50
+    },
     personIcon: {
+        height: 50,
+        width: 50
+    },
+    locationContainer: {
+        backgroundColor: COLORS.blueGray,
+        height: 100,
+        width: 500,
+        borderRadius: 20,
+        padding: 20,
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between"
+    },
+    location: {
+        fontSize: 36,
+        width: 350,
+        fontFamily: "'Zain', sans-serif"
+    },
+    mapsIcon: {
         height: 50,
         width: 50
     },
@@ -164,6 +212,7 @@ const stylesMobile = StyleSheet.create({
     },
     description: {
         fontSize: 36,
+        fontFamily: "'Zain', sans-serif"
     },
     buttonsContainer: {
         display: "flex",
@@ -189,6 +238,7 @@ const stylesMobile = StyleSheet.create({
     },
     buttonText: {
         fontSize: 48,
+        fontFamily: "'Zain', sans-serif",
         color: COLORS.alabaster
     }
 })
@@ -212,40 +262,23 @@ const stylesDesktop = StyleSheet.create({
         flexDirection: "column",
         gap: 10
     },
+    titleOccupancyContainer: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        gap: 20
+    },
     titleContainer: {
         backgroundColor: COLORS.blueGray,
         height: 50,
         borderRadius: 10,
-        width: 200,
+        width: 250,
         justifyContent: "center",
         paddingHorizontal: 10
     },
     title: {
         fontSize: 22,
-    },
-    dateOccupancyContainer: {
-        display: "flex",
-        flexDirection: "row",
-        gap: 10
-    },
-    dateContainer: {
-        backgroundColor: COLORS.blueGray,
-        height: 50,
-        width: 150,
-        borderRadius: 10,
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-        paddingHorizontal: 10
-    },
-    date: {
-        fontSize: 22,
-        width: 100
-    },
-    calendarIcon: {
-        height: 25,
-        width: 25
+        fontFamily: "'Zain', sans-serif"
     },
     occupancyContainer: {
         backgroundColor: COLORS.blueGray,
@@ -260,9 +293,54 @@ const stylesDesktop = StyleSheet.create({
     },
     occupancy: {
         fontSize: 22,
+        fontFamily: "'Zain', sans-serif",
         width: 50
     },
+    startEndContainer: {
+        display: "flex",
+        flexDirection: "row",
+        gap: 10
+    },
+    dateContainer: {
+        backgroundColor: COLORS.blueGray,
+        height: 50,
+        width: 200,
+        borderRadius: 10,
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        paddingHorizontal: 10
+    },
+    date: {
+        fontSize: 22,
+        fontFamily: "'Zain', sans-serif",
+        width: 150
+    },
+    calendarIcon: {
+        height: 25,
+        width: 25
+    },
     personIcon: {
+        height: 25,
+        width: 25
+    },
+    locationContainer: {
+        backgroundColor: COLORS.blueGray,
+        height: 50,
+        width: 300,
+        borderRadius: 10,
+        padding: 10,
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between"
+    },
+    location: {
+        fontSize: 22,
+        width: 350,
+        fontFamily: "'Zain', sans-serif"
+    },
+    mapsIcon: {
         height: 25,
         width: 25
     },
@@ -278,6 +356,7 @@ const stylesDesktop = StyleSheet.create({
     },
     description: {
         fontSize: 16,
+        fontFamily: "'Zain', sans-serif"
     },
     buttonsContainer: {
         display: "flex",
@@ -303,6 +382,7 @@ const stylesDesktop = StyleSheet.create({
     },
     buttonText: {
         fontSize: 16,
+        fontFamily: "'Zain', sans-serif",
         color: COLORS.alabaster
     }
 })

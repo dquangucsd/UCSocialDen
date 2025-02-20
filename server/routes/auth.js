70a4c834a5921347e190d43af5e5b40d816f5afe
dquangucsd <dquang@ucsd.edu>
2025-02-19 20:33:32 -0800

    blocks login if it is not a ucsd.edu email in passport.js and auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -11,22 +11,28 @@ router.get("/google", passport.authenticate("google", { scope: ["profile", "emai
 router.get(
   "/google/callback",
   passport.authenticate("google", { failureRedirect: "/", session: false  }),
-    (req, res) => {
-        if (!req.user) {
+    (req, res) => { // req holds { user, token }, it is the answer from passport.js
+        if (!req.user) { 
             return res.status(401).json({ success: false, message: "Authentication failed" });
+        }
+        
+        // if not sucessful, return error message
+        if (req.user.error) {
+            return res.status(401).json({ success: false, message: req.user.error });
         }
 
         // if success get information for user
 
-        if (req.user.newUser) {
+        if (req.user.newUser) { // redirects you to register
             // reg TODO
-            return res.redirect(`http://localhost:8081/register?email=${req.user.email}`);
+            return res.redirect(`http://localhost:8081/register?email=${req.user.email}`); // first we send new user to register. 
+                                                                                           // after user registers, we send them back to login again
         }
     
         // get token and user information
-        const { token, user } = req.user;
+        const { token, user } = req.user; // login
     
-        // redirect to homepage with token
+        // redirect to homepage with token, meaning you are logged in with the homepage
         res.redirect(`http://localhost:8081?token=${token}&user=${encodeURIComponent(JSON.stringify(user))}`);
     }
 );

8878ee37b1a442e84be5f84cb468f50c36e968d3
Zhang <zhangleyang0904@gmail.com>
2025-02-25 19:13:02 -0800

    aws picture + join

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {getUsers, getUserByEmail, updateUserIntro} = require("../controller/userController");
+const {getUsers, getUserByEmail, updateUserIntro, uploadimage,Upload, joinEvent} = require("../controller/userController");
 
 
 const router = express.Router();
@@ -7,5 +7,7 @@ const router = express.Router();
 router.get("/", getUsers);
 router.get("/:email", getUserByEmail); //: means input
 router.put("/:email/intro", updateUserIntro)
+router.post('/upload/:email', Upload, uploadimage);
+router.post('/:eventId/join', joinEvent);
 
 module.exports = router;
